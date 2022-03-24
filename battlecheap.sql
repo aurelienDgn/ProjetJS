6b2d5cabf47b200f4b07498c6a50fa9edf46ac54
Maxime <maxime.bonnel@student.yncrea.fr>
2022-03-24 11:26:44 +0100

    front

--- battlecheap.sql
+++ battlecheap.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE  IF NOT EXISTS `ciriousgamer` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `ciriousgamer`;
+CREATE DATABASE  IF NOT EXISTS `battlecheap` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `battlecheap`;
 -- MySQL dump 10.13  Distrib 8.0.28, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: battlecheap
@@ -29,7 +29,7 @@ CREATE TABLE `users` (
   `username` varchar(45) NOT NULL,
   `password` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
