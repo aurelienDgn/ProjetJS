15f0294173b3cbfd571cd449cc17eacc4304e2d5
Mehdi <djallal.chafai@student.yncrea.fr>
2022-03-28 23:03:05 +0200

    tour par tour et visualisation attaque joueur

--- Front/JS/mainGame.js
+++ Front/JS/mainGame.js
@@ -10,10 +10,19 @@ listeners.bat();
 
 socket.on("result", (msgTab) => {
 
-    console.log("zee");
-    /*let it = document.getElementById("dfd");
-    for(let i=0;i<msgTab.length;i++){
-        console.log(msgTab[i]+"\n");
-        it.textContent += msgTab[i]+"\n"
-    }*/
+  console.log("zee ", msgTab);
+  if (msgTab =! null && msgTab != -1) {
+    if (msgTab.length > 0) {
+      // console.log("MSGTAB :", msgTab);
+      for (let i = 0; i < msgTab.length; i++) {
+        game.setCase(msgTab[i][0], msgTab[i][1], 1, 1);
+      }
+
+    }
+  }
+  // let it = document.getElementById("dfd");
+
+  // for(let i=0;i<msgTab.length;i++){
+  //     console.log("POOOP ", msgTab[i]+"\n");
+  // }
 })
